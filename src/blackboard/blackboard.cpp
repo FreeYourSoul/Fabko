61456ea2c5deefecc0e8cb67f1507df04f6659af
FyS <ballandfys@protonmail.com>
2023-04-27 13:53:17 +0200

    prepare blackboard abstraction / type erasure

--- src/blackboard/blackboard.cpp
+++ src/blackboard/blackboard.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "blackboard.hh"
+
+namespace fabko {
+
+std::future<request_propositions> blackboard::request_propositions(const std::string& request) {
+  return {};
+}
+
+std::future<decision_status> blackboard::submit_decision(const std::string& decision) {
+  return {};
+}
+
+}
--- src/blackboard/blackboard.hh
+++ src/blackboard/blackboard.hh
@@ -17,8 +17,11 @@ struct proposition {
   std::string id;
 };
 
+using request_propositions = std::optional<std::vector<proposition>>;
+
 struct blackboard_data {
   std::string id;
+  request_propositions propositions;
 };
 
 enum class decision_status {
@@ -34,23 +37,47 @@ enum class decision_status {
 template<typename T>
 concept c_board_com =
     requires(T a) {
-      { a.instantiate_black_board(std::string{}) } -> std::same_as<blackboard_data>;
-      { a.is_request_ready(std::string{}) } -> std::same_as<std::future<std::optional<std::vector<proposition>>>>;
+      { a.instantiate_black_board(std::string{}) } -> std::same_as<std::string>;
+      { a.request_propositions(std::string{}) } -> std::same_as<std::future<request_propositions>>;
       { a.commit_decision(std::string{}) } -> std::same_as<decision_status>;
     } && std::movable<T>;
+
+namespace details {
+class blackboard_impl {
+  virtual blackboard_data instantiate_black_board() = 0;
+};
+
+template<c_board_com BoardCommunication>
+class blackboard_impl_store : public blackboard_impl {
+public:
+  BoardCommunication _bc;
+  blackboard_data _data{};
+
+  blackboard_data instantiate_black_board() override {
+    _data.id = _bc->instantiate_black_board();
+    return _data;
+  }
+};
+
+}// namespace details
 
 /**
  *
  * @tparam BoardCommunication
  */
-template<c_board_com BoardCommunication>
 class blackboard {
 
+private:
 public:
-  explicit blackboard(BoardCommunication&& bc) : _bc(std::make_unique<BoardCommunication>(std::forward<BoardCommunication>(bc))) {}
+  template<c_board_com BoardCommunication>
+  explicit blackboard(BoardCommunication&& bc)
+      : _pimpl(std::make_unique<details::blackboard_impl_store<BoardCommunication>>(std::forward<BoardCommunication>(bc))) {}
+
+  [[nodiscard]] std::future<request_propositions> request_propositions(const std::string& request);
+  [[nodiscard]] std::future<decision_status> submit_decision(const std::string& decision);
 
 private:
-  std::unique_ptr<BoardCommunication> _bc;
+  std::unique_ptr<details::blackboard_impl> _pimpl;
 };
 
 }// namespace fabko
