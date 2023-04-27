f5c1fa6d386bc4188aed84e6ba022c93d66e5f14
FyS <ballandfys@protonmail.com>
2023-04-27 13:53:18 +0200

    Change licensing

--- fabko/datastore/kv_rocksdb.cpp
+++ fabko/datastore/kv_rocksdb.cpp
@@ -1,22 +1,13 @@
-// MIT License
+// Dual Licensing Either :
+// - AGPL
+// or
+// - Subscription license for commercial usage (without requirement of licensing propagation).
+//   please contact ballandfys@protonmail.com for additional information about this subscription commercial licensing.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-//         of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-//         to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//         copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// Created by FyS on 23/04/23. License 2022-2023
 //
-// The above copyright notice and this permission notice shall be included in all
-//         copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//         AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// In the case no license has been purchased for the use (modification or distribution in any way) of the software stack
+// the APGL license is applying.
 //
 
 #include <optional>
