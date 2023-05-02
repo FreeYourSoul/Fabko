5e9571bf283ee6365b655e03abcd6181cbcfc747
FyS <ballandfys@protonmail.com>
2023-05-03 01:22:58 +0200

    ticket #13 : add versionning

---
+++ fabko/version.hh
@@ -0,0 +1,19 @@
+// Dual Licensing Either :
+// - AGPL
+// or
+// - Subscription license for commercial usage (without requirement of licensing propagation).
+//   please contact ballandfys@protonmail.com for additional information about this subscription commercial licensing.
+//
+// Created by FyS on 03/05/23. License 2022-2023
+//
+// In the case no license has been purchased for the use (modification or distribution in any way) of the software stack
+// the APGL license is applying.
+//
+
+#pragma once
+
+#define FABKO_VER_MAJOR 0
+#define FABKO_VER_MINOR 0
+#define FABKO_VER_PATCH 1
+
+#define FABKO_VERSION (FABKO_VER_MAJOR * 10000 + FABKO_VER_MINOR * 100 + FABKO_VER_PATCH)
