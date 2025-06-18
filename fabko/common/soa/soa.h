b49a4ab91cb95308795f5c48a90ab8d6990564d4
FyS <ballandFyS@protonmail.com>
2025-06-18 07:45:40 +0200

    add soa structure

---
+++ fabko/common/soa/soa.h
@@ -0,0 +1,16 @@
+// Dual Licensing Either :
+// - AGPL
+// or
+// - Subscription license for commercial usage (without requirement of licensing propagation).
+//   please contact ballandfys@protonmail.com for additional information about this subscription commercial licensing.
+//
+// Created by fys on 17.06.25. License 2022-2025
+//
+// In the case no license has been purchased for the use (modification or distribution in any way) of the software stack
+// the APGL license is applying.
+//
+
+#ifndef SOA_H
+#define SOA_H
+
+#endif //SOA_H
